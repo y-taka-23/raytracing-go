d9f2846a92c522160528a9ce08f589d35e7e835a
Yuto Takahashi <ytaka23dev@gmail.com>
2021-03-15 06:59:50 +0900

    Output a test image

--- raytracing.go
+++ raytracing.go
@@ -4,7 +4,31 @@ import (
 	"fmt"
 )
 
+const (
+	imageWidth  = 256
+	imageHeight = 256
+)
+
 func Run() error {
-	fmt.Println("Hello, Ray Tracing!")
+
+	fmt.Println("P3")
+	fmt.Printf("%d %d\n", imageWidth, imageHeight)
+	fmt.Println(255)
+
+	for j := imageHeight - 1; j >= 0; j-- {
+		for i := 0; i < imageWidth; i++ {
+
+			r := float64(i) / (imageWidth - 1)
+			g := float64(j) / (imageHeight - 1)
+			b := 0.25
+
+			ir := int(r * 255.999)
+			ig := int(g * 255.999)
+			ib := int(b * 255.999)
+
+			fmt.Printf("%d %d %d\n", ir, ig, ib)
+		}
+	}
+
 	return nil
 }
